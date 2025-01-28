4cf13c7ba391f5f873e67d4f260538321f013064
KarmicSine68 <117841894+KarmicSine68@users.noreply.github.com>
2025-01-28 13:55:22 -0600

    Backing up code progress
    Code reads the float value of the player input. Player doesn't move yet. Variables added to inspector.

--- Assets/Scripts/PlayerScripts/PlayerBehaviour.cs
+++ Assets/Scripts/PlayerScripts/PlayerBehaviour.cs
@@ -1,18 +1,56 @@
+/*****************************************************************************
+ * Author: Brad Dixon
+ * Creation Date: 1/28/2025
+ * File Name: PlayerBehaviour.cs
+ * Brief: Controls the movement and actions of the player
+ * ***************************************************************************/
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerBehaviour : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private InputActionMap actions;
+    private InputAction playerMove, playerJump;
+
+    [Header("Player Movement")]
+
+    [Tooltip("How fast the player's base speed is")]
+    [SerializeField] private float playerSpeed;
+
+    [Tooltip("A multipler for slowing the player down")]
+    [SerializeField] private float slowMultipler;
+
+    [Tooltip("A visual in the inspector to show the player's current speed multipler")]
+    [SerializeField] private float speedMultiplier;
+
+    //A base value for returning the player's speed to normal
+    private const float BASE_MULTIPLER = 1;
+    [SerializeField] private Rigidbody2D rb2d;
+    private float moveValue;
+
+    /// <summary>
+    /// Enables the action map and inputs for the rest of the code
+    /// </summary>
+    private void Awake()
     {
-        
+        actions = GetComponent<PlayerInput>().currentActionMap;
+        actions.Enable();
+
+        playerMove = actions.FindAction("Move");
+        playerJump = actions.FindAction("Jump");
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        
+        MovePlayer();
+    }
+
+    private void MovePlayer()
+    {
+        moveValue = playerMove.ReadValue<float>();
+
+        Debug.Log(moveValue);
     }
 }
