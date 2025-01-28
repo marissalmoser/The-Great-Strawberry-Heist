4f70a9ac71bf218e23ab1d242a3ba1a4bd96f815
KarmicSine68 <117841894+KarmicSine68@users.noreply.github.com>
2025-01-28 14:51:11 -0600

    Player can move and jump

--- Assets/Scripts/PlayerScripts/PlayerBehaviour.cs
+++ Assets/Scripts/PlayerScripts/PlayerBehaviour.cs
@@ -14,6 +14,9 @@ public class PlayerBehaviour : MonoBehaviour
     private InputActionMap actions;
     private InputAction playerMove, playerJump;
 
+    private BoxCollider2D hitbox;
+    [SerializeField] private LayerMask ground;
+
     [Header("Player Movement")]
 
     [Tooltip("How fast the player's base speed is")]
@@ -25,10 +28,15 @@ public class PlayerBehaviour : MonoBehaviour
     [Tooltip("A visual in the inspector to show the player's current speed multipler")]
     [SerializeField] private float speedMultiplier;
 
+    [Tooltip("How far the player jumps into the air")]
+    [SerializeField] private float jumpHeight;
+
     //A base value for returning the player's speed to normal
     private const float BASE_MULTIPLER = 1;
     [SerializeField] private Rigidbody2D rb2d;
     private float moveValue;
+
+    private float isGrounded;
 
     /// <summary>
     /// Enables the action map and inputs for the rest of the code
@@ -40,17 +48,59 @@ public class PlayerBehaviour : MonoBehaviour
 
         playerMove = actions.FindAction("Move");
         playerJump = actions.FindAction("Jump");
+
+        playerJump.performed += PlayerJump_performed;
+
+        speedMultiplier = BASE_MULTIPLER;
+
+        hitbox = GetComponent<BoxCollider2D>();
     }
 
+    /// <summary>
+    /// Executes when the jump button is pressed
+    /// </summary>
+    /// <param name="obj"></param>
+    private void PlayerJump_performed(InputAction.CallbackContext obj)
+    {
+        PlayerJump();
+    }
+
+    /// <summary>
+    /// Executes player code at a fixed rate
+    /// </summary>
     private void FixedUpdate()
     {
         MovePlayer();
     }
 
+    /// <summary>
+    /// Moves the player left and right
+    /// </summary>
     private void MovePlayer()
     {
         moveValue = playerMove.ReadValue<float>();
+        moveValue = moveValue * playerSpeed * speedMultiplier;
 
-        Debug.Log(moveValue);
+        rb2d.velocity = new Vector2(moveValue, rb2d.velocity.y);
+    }
+
+    /// <summary>
+    /// Causes the player to jump when the button is pressed
+    /// </summary>
+    private void PlayerJump()
+    {
+        if(CanJump())
+        {
+            rb2d.AddForce(new Vector2(rb2d.velocity.x, jumpHeight), ForceMode2D.Impulse);
+        }
+    }
+
+    /// <summary>
+    /// Checks if the player is grounded to see if they can jump
+    /// </summary>
+    /// <returns></returns>
+    private bool CanJump()
+    {
+        return Physics2D.BoxCast(hitbox.bounds.center, hitbox.bounds.size * .95f, 0, Vector2.down, 0.1f, ground);
     }
 }
