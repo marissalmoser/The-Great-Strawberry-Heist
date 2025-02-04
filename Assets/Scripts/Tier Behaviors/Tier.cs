5436f67c2da5cbd187f1fd50b239c59a94b44259
Marissa Moser <marissalmoser@gmail.com>
2025-02-04 12:35:44 -0600

    swipe attack works

--- Assets/Scripts/Tier Behaviors/Tier.cs
+++ Assets/Scripts/Tier Behaviors/Tier.cs
@@ -3,7 +3,8 @@
 // Author :            Marissa Moser
 // Creation Date :     01/30/2025
 //
-// Brief Description : 
+// Brief Description : Contains functionality that is specific per tier. Such
+    functinoality includes camera functions and the swipe behavior
 *****************************************************************************/
 
 using System.Collections;
@@ -15,42 +16,62 @@ public class Tier : MonoBehaviour
 {
     [SerializeField] private CinemachineVirtualCamera tierCam;
     [SerializeField] private GameObject tierSpawnPt;
-    [SerializeField] private AnimationCurve _turnEaseCurve;
+    [SerializeField] private AnimationCurve _swipeEaseCurve;
+    private CinemachineBasicMultiChannelPerlin _perlinNoise;
 
-
-    void Awake()
+    void Start()
     {
-        
+        _perlinNoise = tierCam.GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>();
     }
 
-    public void DiableCam()
+    /// <summary>
+    /// Disables the tier camera by seting it active and removing it's parent from
+    /// the tier so that its position isn't swiped.
+    /// </summary>
+    public void DisableCam()
     {
         tierCam.gameObject.SetActive(false);
+        tierCam.gameObject.transform.SetParent(null, true);
     }
 
+    /// <summary>
+    /// Call to shake the camera in this tier for the duration
+    /// </summary>
+    public void ShakeCam(float amplitude, float frequency, float duration)
+    {
+        StartCoroutine(Shake(amplitude, frequency, duration));
+    }
+
+    //gets the transform of the spawn point in this tier
     Transform GetTierSpawn()
     {
         return tierSpawnPt.transform;
     }
 
+    /// <summary>
+    /// Starts swipe coroutine
+    /// </summary>
     public void Swipe()
     {
         StartCoroutine(MoveTier());
     }
 
+    /// <summary>
+    /// Moves the tier off screen based on the swipe ease curve
+    /// </summary>
     private IEnumerator MoveTier()
     {
         float timeElapsed = 0f;
-        float totalDuration = _turnEaseCurve.keys[_turnEaseCurve.length - 1].time;
+        float totalDuration = _swipeEaseCurve.keys[_swipeEaseCurve.length - 1].time;
 
         float startPositionX = transform.position.x;
 
         //randomply decides a direction
-        float targetPositionX = Random.Range(0,2) > 1 ? startPositionX - 40f : startPositionX + 40f;
+        float targetPositionX = Random.Range(0,2) >= 1 ? startPositionX - 40f : startPositionX + 40f;
 
         while (timeElapsed < totalDuration)
         {
-            float t = _turnEaseCurve.Evaluate(timeElapsed);
+            float t = _swipeEaseCurve.Evaluate(timeElapsed);
 
             startPositionX = Mathf.Lerp(startPositionX, targetPositionX, t);
 
@@ -61,5 +82,28 @@ public class Tier : MonoBehaviour
             yield return null;
         }
         transform.position = new Vector3(startPositionX, transform.position.y, 0);
+    }
+
+    /// <summary>
+    /// Call coroutine to shake a camera's noise component for the duration at the 
+    /// specified amplitude and frequency.
+    /// </summary>
+    private IEnumerator Shake(float amplitude, float frequency, float duration)
+    {
+        // Set the noise parameters
+        _perlinNoise.m_AmplitudeGain = amplitude;
+        _perlinNoise.m_FrequencyGain = frequency;
+
+        float elapsed = 0.0f;
+
+        while (elapsed < duration)
+        {
+            elapsed += Time.deltaTime;
+            yield return null;
+        }
+
+        // Reset noise parameters
+        _perlinNoise.m_AmplitudeGain = 0f;
+        _perlinNoise.m_FrequencyGain = 0f;
     }
 }
--- Assets/Scripts/Tier Behaviors/TierManager.cs
+++ Assets/Scripts/Tier Behaviors/TierManager.cs
@@ -4,6 +4,7 @@
 // Creation Date :     01/30/2025
 //
 // Brief Description : Derived from Sigleton Monobehavior. Reference using TierManager.Instance
+    contains functionality to control the tiers.
 *****************************************************************************/
 using System.Collections;
 using System.Collections.Generic;
@@ -12,9 +13,19 @@ using UnityEngine.Android;
 
 public class TierManager : Singleton<TierManager>
 {
+    [Tooltip("Every tier game object should be added to this list i norder, with the" +
+        "bottom tier as index 0.")]
     [SerializeField] List<GameObject> cakeTiers;
     List<Tier> tiers = new List<Tier>();
+
     private int currentTier = 0;    //0 is bottom tier
+
+    [Tooltip("Edit this to change how big each shake is when a tier is swiped")]
+    [SerializeField] float tierCamShakeAmplitude;
+    [Tooltip("Edit this to change how often the camera shakes when a tier is swiped")]
+    [SerializeField] float tierCamShakeFrequency;
+    [Tooltip("Edit this to change how long the camera shakes when a tier is swiped")]
+    [SerializeField] float tierCamShakeDuration;
 
     protected override void Awake()
     {
@@ -25,29 +36,52 @@ public class TierManager : Singleton<TierManager>
         {
             tiers.Add(tier.GetComponent<Tier>());
         }
-
-        //starting tier count?
     }
 
+    /// <summary>
+    /// Will be reomoved after testing is sufficient
+    /// </summary>
     void Update()
     {
         if(Input.GetKeyDown(KeyCode.Space))
         {
-            SwipeTier();
+            SwipeTier(tierCamShakeDuration);
         }
     }
 
     /// <summary>
     /// Called when the cat swipes the bottom cake tier
     /// </summary>
-    public void SwipeTier()
+    public void SwipeTier(float duration)
     {
-        //move player to next tier
-        if(currentTier == 0)
-        {
-            //TODO: call move player somehow
-            tiers[0].DiableCam();
+        StartCoroutine(SwipeCoroutine(duration));
+    }
 
+    /// <summary>
+    /// Called when the payer passes into the next cake tier
+    /// </summary>
+    public void NextTier()
+    {
+        tiers[currentTier].DisableCam();
+        currentTier++;
+        print(currentTier);
+    }
+
+    /// <summary>
+    /// Swipes the tier from the cake. Camera with shake for the duration before the
+    /// sjake is performed.
+    /// </summary>
+    private IEnumerator SwipeCoroutine(float duration)
+    {
+        tiers[0].ShakeCam(tierCamShakeAmplitude, tierCamShakeFrequency, duration);
+
+        yield return new WaitForSeconds(duration);
+
+        //move player to next tier
+        if (currentTier == 0)
+        {
+            //TODO: call move player somehow, get spawn transition from tier 1
+            tiers[0].DisableCam();
         }
         else
         {
@@ -58,17 +92,5 @@ public class TierManager : Singleton<TierManager>
 
         cakeTiers.RemoveAt(0);
         tiers.RemoveAt(0);
-
-        //TODO: Camera shake
-    }
-
-    /// <summary>
-    /// Called when the payer passes into the next cake tier
-    /// </summary>
-    public void NextTier()
-    {
-        tiers[currentTier].DiableCam();
-        currentTier++;
-        print(currentTier);
     }
 }
