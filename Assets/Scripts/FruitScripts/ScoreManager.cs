0d4e6e709352799196607c9a1123ff3134b4a7cc
DalstenYan <dyan@mail.bradley.edu>
2025-02-28 15:47:11 -0600

    Vitality Functionality
    * Added Vitality Bar with temporary breakpoints and also the Multiplier Text
    * Changed Fruit Prefabs to include Vitality values
    * Multiplier will now change according to your Vitality, and you can lose Vitality from public function PlayerHit()
    * Partially Organized the Canvas Prefab (This can be overriden later based on design team's needs)
    - DY

--- Assets/Scripts/FruitScripts/ScoreManager.cs
+++ Assets/Scripts/FruitScripts/ScoreManager.cs
@@ -1,6 +1,7 @@
 /*****************************************************************************
 // File Name :         FruitCollect.cs
 // Author :            Kadin Harris
+// Contributors:       Dalsten Yan
 // Creation Date :     01/30/2025
 //
 // Brief Description : Updates the score and multiplier for each fruit collected.
@@ -11,13 +12,33 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.SocialPlatforms.Impl;
+using UnityEngine.UI;
 
 public class ScoreManager : Singleton<ScoreManager>
 {
-    public TMP_Text ScoreText;
+    public TMP_Text ScoreText, MultiplierText;
+    [Tooltip("The Fill Image from the multiplier bar")]
+    public Image BarFillImage;
+
+    [SerializeField]
+    [Tooltip("The Multiplier magnitudes")]
+    List<float> breakpoints;
     
     //The total score integer
     private int Totalscore;
+
+    //total vitality amount
+    private int Vitalitymeter = 0;
+
+    [SerializeField]
+    [Tooltip("Maximum Vitality Meter")]
+    private int maxVitalityMeter = 100;
+    [SerializeField]
+    [Tooltip("The Mutiplier will change every ___ vitality points")]
+    private int vitalityBreakpointInterval = 30;
+    [SerializeField]
+    [Tooltip("How much vitality points decrease on player being hit")]
+    private int vitalityDecreasePoints = 10;
     
     // Multiplier that can be changed in the Inspector
     [SerializeField]
@@ -40,13 +61,33 @@ public class ScoreManager : Singleton<ScoreManager>
     /// <summary>
     /// Adds to total score when a fruit is collected
     /// </summary>
-    public void AddScore(int amount)
+    public void AddScore(int scoreAmt, int vitalityAmt)
     {
-        Totalscore += Mathf.RoundToInt(amount * multiplier);
+        Totalscore += Mathf.RoundToInt(scoreAmt * multiplier);
+        Vitalitymeter = Mathf.Max(vitalityAmt + Vitalitymeter, maxVitalityMeter);
         //Debug.Log("Updated Score: " + Totalscore);
-        //Debug.Log("Amount: " + amount);
-        //Debug.Log("Multiplier: " + multiplier);
+        //Debug.Log("Amount: " + amount
+        //Debug.Log("Vitality Meter: " + Vitalitymeter + " | Vitality Progress: " + vitalityProgress);
 
         ScoreText.text = "Score: " + Totalscore.ToString();
+        ChangeVitality();
+    }
+
+    private void ChangeVitality() 
+    {
+        //Vitality Logic Change
+        int breakpointMultiplierIndex = Mathf.FloorToInt(Vitalitymeter / vitalityBreakpointInterval);
+        ChangeMultiplier(breakpoints[breakpointMultiplierIndex]);
+
+        //Vitality UI change
+        BarFillImage.fillAmount = (float)Vitalitymeter / maxVitalityMeter;
+        MultiplierText.text = multiplier + "x";
+    }
+
+    [ContextMenu("Player Hit")]
+    public void PlayerHit() 
+    {
+        Vitalitymeter = Mathf.Max(Vitalitymeter - vitalityDecreasePoints, 0);
+        ChangeVitality();
     }
 }
