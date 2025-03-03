018782476c0ba2069d863d10e83156dd7f817f08
ablumhardt04 <ablumhardt@mail.bradley.edu>
2025-03-02 20:19:54 -0600

    It's better now

--- Assets/Scripts/PlayerScripts/PlayerBehaviour.cs
+++ Assets/Scripts/PlayerScripts/PlayerBehaviour.cs
@@ -167,8 +167,12 @@ public class PlayerBehaviour : MonoBehaviour
     /// <returns></returns>
     private bool CanJump()
     {
-        return (rb2d.velocity.y < 0.1f) && 
-            Physics2D.BoxCast(hitbox.bounds.center, hitbox.bounds.size * .95f, 0, Vector2.down, 0.1f, ground);
+        var hit = Physics2D.BoxCast(hitbox.bounds.center, hitbox.bounds.size * .95f, 0, Vector2.down, 0.1f, ground);
+        if (hit.collider != null)
+        {
+            return hitbox.bounds.min.y > hit.collider.bounds.max.y;
+        }
+        return false;
     }
 
     /// <summary>
