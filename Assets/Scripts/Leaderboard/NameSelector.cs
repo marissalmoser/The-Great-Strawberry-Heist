00bc5ce06e94e16513e77bcc840406acd7360a0f
KarmicSine68 <117841894+KarmicSine68@users.noreply.github.com>
2025-03-14 14:40:30 -0500

    Time to start calling scores

--- Assets/Scripts/Leaderboard/NameSelector.cs
+++ Assets/Scripts/Leaderboard/NameSelector.cs
@@ -18,8 +18,8 @@ public class NameSelector : MonoBehaviour
     bool navigating = false;
 
     int nameIndex, charIndex;
-    char[] letters = new char[] {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
-    'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+    char[] letters = new char[] {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
+    'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
 
     [SerializeField] private float navDelay;
     [SerializeField] private List<TMP_Text> playerLetters = new List<TMP_Text>();
